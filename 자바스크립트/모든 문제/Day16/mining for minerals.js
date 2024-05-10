8a6b5656d217ffdf39d84ad0f2543ba91e33091e
Jangchaeyun <zzangsally@naver.com>
2024-05-10 17:41:23 +0900

    0510

---
+++ 자바스크립트/모든 문제/Day16/mining for minerals.js
@@ -0,0 +1,32 @@
+function solution(picks, minerals) {
+  var answer = 0;
+  var cnrCount = Math.ceil(minerals.length / 5);
+  let maxLen = picks.reduce((a, b) => a + b);
+  if (maxLen === 0) return 0;
+  minerals = minerals.splice(0, maxLen * 5);
+  var arr = [];
+  for (var i = 0; i < cnrCount; i++) {
+    let obj = { diamond: 0, iron: 0, stone: 0 };
+
+    minerals.splice(0, 5).map((element) => {
+      obj[element]++;
+    });
+    arr.push([
+      obj.diamond + obj.iron + obj.stone,
+      obj.diamond * 5 + obj.iron + obj.stone,
+      obj.diamond * 25 + obj.iron * 5 + obj.stone,
+    ]);
+  }
+
+  arr = arr.sort((a, b) => b[2] - a[2]);
+  for (var i = 0; i < picks.length; i++) {
+    var pickCount = picks[i];
+    while (pickCount--) {
+      if (arr.length === 0) {
+        return answer;
+      }
+      answer += arr.shift()[i];
+    }
+  }
+  return answer;
+}
