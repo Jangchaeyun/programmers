758535e28b2ba98a0933a070a2f6a699233a58ef
Jangchaeyun <zzangsally@naver.com>
2024-05-27 09:57:54 +0900

    0527

---
+++ 자바스크립트/모든 문제/Day33/Performance review.js
@@ -0,0 +1,19 @@
+function solution(scores) {
+  const wanho = scores[0];
+  scores.sort((a, b) => (a[0] === b[0] ? a[1] - b[1] : b[0] - a[0]));
+  let answer = 1;
+  let maxScore = 0;
+  const wanhoSum = wanho[0] + wanho[1];
+
+  for (let score of scores) {
+    if (score[1] < maxScore) {
+      if (score === wanho) return -1;
+    } else {
+      maxScore = Math.max(maxScore, score[1]);
+      if (score[0] + score[1] > wanhoSum) {
+        answer++;
+      }
+    }
+  }
+  return answer;
+}
---
+++ 자바스크립트/모든 문제/Day33/Sum of continuous pulse subsequences.js
@@ -0,0 +1,21 @@
+function solution(sequence) {
+  let answer = 0;
+  const dpPos = [];
+  const dpNeg = [];
+
+  for (let i = 0; i < sequence.length; i++) {
+    const s = sequence[i];
+    if (i === 0) {
+      dpPos.push(s);
+      dpNeg.push(-s);
+    } else if (i % 2 === 0) {
+      dpPos.push(Math.max(dpPos[i - 1] + s, s));
+      dpNeg.push(Math.max(dpNeg[i - 1] - s, -s));
+    } else {
+      dpPos.push(Math.max(dpPos[i - 1] - s, -s));
+      dpNeg.push(Math.max(dpNeg[i - 1] + s, s));
+    }
+    answer = Math.max(answer, dpPos[i], dpNeg[i]);
+  }
+  return answer;
+}
